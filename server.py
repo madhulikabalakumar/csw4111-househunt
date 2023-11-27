15342ace926f2732acb8aaded90c414a42d5a432
Madhulika <mb5144@columbia.edu>
2023-11-27 20:02:51 +0000

    validation

--- server.py
+++ server.py
@@ -516,7 +516,7 @@ if __name__ == "__main__":
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
   @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=4115, type=int)
+  @click.argument('PORT', default=4112, type=int)
   def run(debug, threaded, host, port):
     """
     This function handles command line parameters.
